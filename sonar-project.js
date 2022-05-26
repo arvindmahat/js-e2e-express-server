b3ab05268a65ae200e2ac73f81b3642af1c0deaf
Ubuntu <jenkinsnode1@node1.kohkczsqjcgu1csqtt2e21adzb.px.internal.cloudapp.net>
2022-05-26 21:19:28 +0000

    Added fix

--- sonar-project.js
+++ sonar-project.js
@@ -3,6 +3,7 @@ const sonarqubeScanner = require('sonarqube-scanner');
        serverUrl: 'http://13.70.134.118:9000',
        options : {
        'sonar.sources': '.',
+       'sonar.login': 'e895594452295a935aa60d44fb464ce744077c4d',
        'sonar.inclusions' : 'src/**' // Entry point of your code
        }
      }, () => {});
